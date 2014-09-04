f9f109a2d6bdddbd7017bd91a9ab16c7f68b3712
hackpascal <hackpascal@qq.com>
2014-09-04 23:33:33 +0800

    固定代码修订版本

--- feeds/luci/build/mkrevision.sh
+++ feeds/luci/build/mkrevision.sh
@@ -9,13 +9,15 @@ TOPDIR="${0%mkrevision.sh}"
 
 (
 	cd "$TOPDIR"
-	if svn info >/dev/null 2>/dev/null; then
-		revision="svn-r$(LC_ALL=C svn info | sed -ne 's/^Revision: //p')"
+	if cat ../version >/dev/null 2>/dev/null; then
+		revision="svn-$(cat ../version)"
 	elif git log -1 >/dev/null 2>/dev/null; then
 		revision="svn-r$(LC_ALL=C git log -1 | sed -ne 's/.*git-svn-id: .*@\([0-9]\+\) .*/\1/p')"
 		if [ "$revision" = "svn-r" ]; then
 			revision="git-$(LC_ALL=C git log -1 --pretty=%h)"
 		fi
+	elif svn info >/dev/null 2>/dev/null; then
+		revision="svn-r$(LC_ALL=C svn info | sed -ne 's/^Revision: //p')"
 	else
 		revision="unknown"
 	fi
