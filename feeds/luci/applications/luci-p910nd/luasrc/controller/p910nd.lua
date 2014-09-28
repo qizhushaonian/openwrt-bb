8009a989320c135d4b76aeabd9e06cbc262fd18f
hackpascal <hackpascal@qq.com>
2014-09-28 13:51:49 +0800

    修改 luci-p910nd 包

--- feeds/luci/applications/luci-p910nd/luasrc/controller/p910nd.lua
+++ feeds/luci/applications/luci-p910nd/luasrc/controller/p910nd.lua
@@ -1,17 +1,18 @@
 --[[
+LuCI - Lua Configuration Interface
 
-LuCI UVC Streamer
-(c) 2008 Yanira <forum-2008@email.de>
+Copyright 2008 Steven Barth <steven@midlink.org>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 $Id$
-
 ]]--
+
+require("luci.sys")
 
 module("luci.controller.p910nd", package.seeall)
 
@@ -22,6 +23,5 @@ function index()
 
 	local page
 
-	page = entry({"admin", "services", "p910nd"}, cbi("p910nd"), _("p910nd - Printer server"), 60)
-	page.dependent = true
+	page = entry({"admin", "services", "p910nd"}, cbi("p910nd"), _("USB Printer Server"), 50)
 end
--- feeds/luci/applications/luci-p910nd/luasrc/model/cbi/p910nd.lua
+++ feeds/luci/applications/luci-p910nd/luasrc/model/cbi/p910nd.lua
@@ -1,33 +1,104 @@
 --[[
+LuCI - Lua Configuration Interface
 
-LuCI p910nd
-(c) 2008 Yanira <forum-2008@email.de>
-(c) 2012 Jo-Philipp Wich <jow@openwrt.org>
+Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2005-2013 hackpascal <hackpascal@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
+$Id$
 ]]--
 
+require "luci.util"
 local uci = luci.model.uci.cursor_state()
 local net = require "luci.model.network"
-local m, s, p, b
 
-m = Map("p910nd", translate("p910nd - Printer server"),
-        translatef("First you have to install the packages to get support for USB (kmod-usb-printer) or parallel port (kmod-lp)."))
+m = Map("p910nd", translate("USB Printer Server"),
+	translate("Shares multiple USB printers via TCP/IP.<br />When modified bingings, re-plug usb connectors to take effect.<br />This module requires kmod-usb-printer."))
+
+function hex_align(hex, num)
+	local len = num - string.len(hex)
+
+	return string.rep("0", len) .. hex
+end
+
+function detect_usb_printers()
+	local data = {}
+
+	local lps = luci.util.execi("/usr/sbin/usb_printer_detect")
+
+	for value in lps do
+		local row = {}
+
+		--[[
+			detectlp 的输出格式：
+			设备名，VID/PID/?,描述，型号
+		]]--
+
+		local pos = string.find(value, ",")
+
+		local devname = string.sub(value, 1, pos - 1)
+
+		local value = string.sub(value, pos + 1, string.len(value))
+
+		pos = string.find(value, ",")
+		local product = string.sub(value, 1, pos - 1)
+
+		value = string.sub(value, pos + 1, string.len(value))
+
+		pos = string.find(value, ",")
+		local model = string.sub(value, 1, pos - 1)
+
+		local name = string.sub(value, pos + 1, string.len(value))
+
+		pos = string.find(product, "/");
+
+		local vid = string.sub(product, 1, pos - 1)
+
+		local pid = string.sub(product, pos + 1, string.len(product))
+
+		pos = string.find(pid, "/")
+		pid = string.sub(pid, 1, pos - 1)
+
+		row["description"] = name
+		row["model"] = model
+		row["id"] = hex_align(vid, 4) .. ":" .. hex_align(pid, 4)
+		row["name"] = devname
+		row["product"] = product
+
+		table.insert(data, row)
+	end
+
+	return data
+end
+
+local printers = detect_usb_printers()
+
+v = m:section(Table, printers, translate("Detected printers"))
+
+v:option(DummyValue, "description", translate("Description"))
+v:option(DummyValue, "model", translate("Printer Model"))
+v:option(DummyValue, "id", translate("VID/PID"))
+v:option(DummyValue, "name", translate("Device Name"))
 
 net = net.init(m.uci)
 
-s = m:section(TypedSection, "p910nd", translate("Settings"))
+s = m:section(TypedSection, "printer", translate("Bindings"))
 s.addremove = true
 s.anonymous = true
 
 s:option(Flag, "enabled", translate("enable"))
 
-s:option(Value, "device", translate("Device")).rmempty = true
+d = s:option(Value, "device", translate("Device"))
+d.rmempty = true
+
+for key, item in ipairs(printers) do
+	d:value(item["product"], item["description"] .. " [" .. item["id"] .. "]")
+end
 
 b = s:option(Value, "bind", translate("Interface"), translate("Specifies the interface to listen on."))
 b.template = "cbi/network_netlist"
@@ -50,8 +121,8 @@ end
 
 p = s:option(ListValue, "port", translate("Port"), translate("TCP listener port."))
 p.rmempty = true
-for i=0,9 do
-	p:value(i, 9100+i)
+for i = 0, 9 do
+	p:value(i, 9100 + i)
 end
 
 s:option(Flag, "bidirectional", translate("Bidirectional mode"))
